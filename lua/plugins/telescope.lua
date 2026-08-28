109728ef0e2acb73266d394776f84739ea944e0d
Milosh Zechevich <milosh@zechevich.com>
2026-08-28 03:21:49 +0200

    Removed comment

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,5 @@
 return {
   "nvim-telescope/telescope.nvim",
-  -- Removed tag = '0.1.8' to track master fixes for Neovim 0.12
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
     { "<leader>ff", "<cmd>Telescope find_files<CR>", mode = "n", noremap = true, desc = "Run telescope" },
